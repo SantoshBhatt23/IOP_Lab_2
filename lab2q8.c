76adec634dd7acb424c577690847ba0e7ab8e47f
SantoshBhatt23 <bhattsantosh030@gmail.com>
2025-09-17 14:32:04 +0530

    Update lab2q8.c

--- lab2q8.c
+++ lab2q8.c
@@ -4,13 +4,14 @@ int main()
 {
     int n , p , r =1,i ;
     printf("enter the number : ");
-    scanf("%d",n);
+    scanf("%d",&n);
     printf("enter the power: ");
-    scanf("%d",p);
+    scanf("%d",&p);
     for (i = 1 ; i<=p ; i++)
     {
-        r = r * n;
+        r *= n;
     }
     printf("result = %d",r);
     return 0;
+
 }
